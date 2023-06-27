b6a4a82d4167f4ba8fe466861d836b25e839504b
BNdungu <bryannganga909@gmail.com>
2023-06-27 23:25:23 +0300

    _

--- app.js
+++ app.js
@@ -1,23 +1,3 @@
-// const mqtt = require('mqtt')
-
-// const client = mqtt.connect({
-// 	protocal:'mqtt',
-// 	host:'13.48.47.239',
-// 	port:1883
-// })
-
-// client.on('connect',() => {
-// 	client.subscribe('topic',(err) => {
-// 		if(!err) {
-// 			console.log('Subscribed')
-// 		}
-// 	})
-// })
-
-// client.on('message', (topic,message) => {
-// 	console.log(message.toString())
-// })
-
 const {connect,client} = require('./mqtt/sub')
 
 connect('Hello')
